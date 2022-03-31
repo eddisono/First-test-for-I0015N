8c676880599adabdcce5dcc433df02b7d7dea25d
eddgru6 <eddgru-6@student.ltu.se>
2022-03-31 22:36:16 +0200

    Every necessary file should now be included

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Add two numbers")
+    void add() {
+        assertEquals(5, Calculator.add(2,3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+        assertEquals(2, Calculator.subtract(3,1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        assertEquals(10, Calculator.multiply(5, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+        assertEquals(10, Calculator.divide(100, 10));
+    }
+}

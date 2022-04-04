023e0e7178287dc067de8ce02b395e6aecd47f07
eddgru6 <eddgru-6@student.ltu.se>
2022-04-04 18:48:53 +0200

    Fixed @Test and @DisplayName for names on the tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
@@ -11,17 +11,20 @@ class CalculatorTest {
         assertEquals(5, Calculator.add(2,3));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Subtract two numbers")
     void subtract() {
         assertEquals(2, Calculator.subtract(3,1));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Multiply two numbers")
     void multiply() {
         assertEquals(10, Calculator.multiply(5, 2));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Divide two numbers")
     void divide() {
         assertEquals(10, Calculator.divide(100, 10));
     }
